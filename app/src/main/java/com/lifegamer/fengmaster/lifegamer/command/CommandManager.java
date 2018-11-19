08058ae37f0fb8458fff8e20482f837b2edb4db8
FengMaster <554286238@qq.com>
2018-11-19 21:44:56 +0800

    解决多个命令提示冲突问题

--- app/src/main/java/com/lifegamer/fengmaster/lifegamer/command/CommandManager.java
+++ app/src/main/java/com/lifegamer/fengmaster/lifegamer/command/CommandManager.java
@@ -11,9 +11,37 @@ import com.lifegamer.fengmaster.lifegamer.util.ViewUtil;
 
 public class CommandManager {
 
+    /**
+     * 命令集头部
+     */
+    private boolean isHead=true;
+
+    /**
+     * 命令集中间的事件,不会提示
+     */
+    private boolean hide=true;
+
     public void executeCommand(ICommand command){
-        command.execute();
-        if (command.isShow()){
+        //如果是事件集的头部
+        if (isHead){
+            /**
+             * 为了解决如下问题:
+             *  如果一个命令调用了其他命令,可能会出现前一个命令的提示还未显示完全,就被子命令的提示覆盖
+             *
+             *          这里采用解决方案是:只显示命令集中第一个命令的提示
+             *
+             * */
+            isHead=false;
+            command.execute();
+            isHead=true;
+            hide=false;
+        }else {
+            hide=true;
+            command.execute();
+        }
+
+
+        if (command.isShow()&&!hide){
             if (command.isUndoable()){
                 //可取消
                 ViewUtil.showSnack(command.getName(), Snackbar.LENGTH_LONG, command.getUndoActionName(), view -> command.undo());
