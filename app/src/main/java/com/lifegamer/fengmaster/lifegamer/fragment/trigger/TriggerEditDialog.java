bf880bf6b47a6f323950cf351704084b75ec040c
feng-master <554286238@qq.com>
2019-01-11 18:18:57 +0800

    触发器UI暂存

---
+++ app/src/main/java/com/lifegamer/fengmaster/lifegamer/fragment/trigger/TriggerEditDialog.java
@@ -0,0 +1,14 @@
+package com.lifegamer.fengmaster.lifegamer.fragment.trigger;
+
+import com.lifegamer.fengmaster.lifegamer.fragment.base.BaseDialogFragment;
+
+/**
+ * 触发器编辑页面
+ * Created by FengMaster on 19/01/11.
+ */
+public class TriggerEditDialog extends BaseDialogFragment {
+
+
+
+
+}
